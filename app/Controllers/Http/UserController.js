bfea696dcc5636c43c240f6e8edbb942ab50d599
Yohan <elyohan14@gmail.com>
2024-02-26 23:03:40 -0400

    Prueba

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -2,7 +2,7 @@
 
 const User = use('App/Models/User')
 const Permission = use('App/Models/Permission')
-const myFunction = require('../../FireAdmin/admin.js')
+const { myFunction } = use('../../FireAdmin/admin.js')
 // import myFunction from '../../FireAdmin/admin.js'
 
 class UserController {
--- app/FireAdmin/admin.js
+++ app/FireAdmin/admin.js
@@ -2,40 +2,42 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./homeplusnotify-f6088-firebase-adminsdk-556z1-ad80833cf6.json");
 
-export default function myFunction(token, theBody) {
+module.exports = {
+  myFunction(token, theBody) {
 
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      // Otras configuraciones según sea necesario
-    });
+    if (!admin.apps.length) {
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        // Otras configuraciones según sea necesario
+      });
+    }
+    // This registration token comes from the client FCM SDKs.
+    const registrationToken = token;
+
+    const message = {
+      notification: {
+        title: 'HomePlus',
+        body: theBody
+      },
+      webpush: {
+        fcmOptions: {
+          link: '/?breakingnews'
+        }
+      },
+      token: registrationToken
+    };
+
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    admin.messaging().send(message)
+      .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
   }
-  // This registration token comes from the client FCM SDKs.
-  const registrationToken = token;
-
-  const message = {
-    notification: {
-      title: 'HomePlus',
-      body: theBody
-    },
-    webpush: {
-      fcmOptions: {
-        link: '/?breakingnews'
-      }
-    },
-    token: registrationToken
-  };
-
-  // Send a message to the device corresponding to the provided
-  // registration token.
-  admin.messaging().send(message)
-    .then((response) => {
-      // Response is a message ID string.
-      console.log('Successfully sent message:', response);
-    })
-    .catch((error) => {
-      console.log('Error sending message:', error);
-    });
 }
 
 
