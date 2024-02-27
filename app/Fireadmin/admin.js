4f1abd6fde729fa157ec7edfcebdca15db605b2d
Yohan <elyohan14@gmail.com>
2024-02-26 23:11:40 -0400

    Prueba

--- app/Fireadmin/admin.js
+++ app/Fireadmin/admin.js
@@ -2,42 +2,40 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./homeplusnotify-f6088-firebase-adminsdk-556z1-ad80833cf6.json");
 
-module.exports = {
-  myFunction(token, theBody) {
+module.exports = function myFunction(token, theBody) {
 
-    if (!admin.apps.length) {
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        // Otras configuraciones según sea necesario
-      });
-    }
-    // This registration token comes from the client FCM SDKs.
-    const registrationToken = token;
-
-    const message = {
-      notification: {
-        title: 'HomePlus',
-        body: theBody
-      },
-      webpush: {
-        fcmOptions: {
-          link: '/?breakingnews'
-        }
-      },
-      token: registrationToken
-    };
-
-    // Send a message to the device corresponding to the provided
-    // registration token.
-    admin.messaging().send(message)
-      .then((response) => {
-        // Response is a message ID string.
-        console.log('Successfully sent message:', response);
-      })
-      .catch((error) => {
-        console.log('Error sending message:', error);
-      });
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      // Otras configuraciones según sea necesario
+    });
   }
+  // This registration token comes from the client FCM SDKs.
+  const registrationToken = token;
+
+  const message = {
+    notification: {
+      title: 'HomePlus',
+      body: theBody
+    },
+    webpush: {
+      fcmOptions: {
+        link: '/?breakingnews'
+      }
+    },
+    token: registrationToken
+  };
+
+  // Send a message to the device corresponding to the provided
+  // registration token.
+  admin.messaging().send(message)
+    .then((response) => {
+      // Response is a message ID string.
+      console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+      console.log('Error sending message:', error);
+    });
 }
 
 
